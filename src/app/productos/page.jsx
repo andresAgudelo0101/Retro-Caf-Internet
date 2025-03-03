6238c8b9128abe0d4fd71602b90cbc05230437e9
andresAgudelo0101 <89613699+andresAgudelo0101@users.noreply.github.com>
2025-03-03 16:59:04 +0000

    estructura grid de productos

--- src/app/productos/page.jsx
+++ src/app/productos/page.jsx
@@ -4,11 +4,31 @@ import Footer from "../components/footer";
 export default function Productos() {
   return (
     <>
-    <Header />
-    <div className="w-100 h-screen bg-GRAY-500 flex justify-center items-center">
-        <h1>TE VENDO ESTA PSP QUE LEE JUEGOS DE NINTENDO 65</h1>
+      <Header />
+      <div className="w-full h-auto min-h-screen bg-gray-500 grid justify-center items-center p-6">
+        <div class="grid grid-cols-2  md:grid-cols-3 gap-9 px-4 py-3  ">
+          <div class="w-32 h-24 bg-gray-300 rounded-xl"></div>
+          <div class="w-32 h-24 bg-gray-300 rounded-xl"></div>
+          <div class="w-32 h-24 bg-gray-300 rounded-xl"></div>
+          <div class="w-32 h-24 bg-gray-300 rounded-xl"></div>
+          <div class="w-32 h-24 bg-gray-300 rounded-xl"></div>
+          <div class="w-32 h-24 bg-gray-300 rounded-xl"></div>
+          <div class="w-32 h-24 bg-gray-300 rounded-xl"></div>
+          <div class="w-32 h-24 bg-gray-300 rounded-xl"></div>
+          <div class="w-32 h-24 bg-gray-300 rounded-xl"></div>
+          <div class="w-32 h-24 bg-gray-300 rounded-xl"></div>
+          <div class="w-32 h-24 bg-gray-300 rounded-xl"></div>
+          <div class="w-32 h-24 bg-gray-300 rounded-xl"></div>
+          <div class="w-32 h-24 bg-gray-300 rounded-xl"></div>
+          <div class="w-32 h-24 bg-gray-300 rounded-xl"></div>
+          <div class="w-32 h-24 bg-gray-300 rounded-xl"></div>
+          <div class="w-32 h-24 bg-gray-300 rounded-xl"></div>
+          <div class="w-32 h-24 bg-gray-300 rounded-xl"></div>
+          <div class="w-32 h-24 bg-gray-300 rounded-xl"></div>
+          <div class="w-32 h-24 bg-gray-300 rounded-xl"></div>
+        </div>
       </div>
-    <Footer/>
+      <Footer />
     </>
   );
-}   
+}
