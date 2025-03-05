dd873bd4b1e06fec6b5ccfaf46de600f78e6d698
andresAgudelo0101 <89613699+andresAgudelo0101@users.noreply.github.com>
2025-03-05 17:14:59 +0000

    mapa leaflet agragado parece estable

--- src/app/contacto/page.jsx
+++ src/app/contacto/page.jsx
@@ -2,65 +2,31 @@
 
 import Footer from "../components/footer";
 import Header from "../components/header";
-import { useRef, useEffect } from "react";
-import mapboxgl from "mapbox-gl";
+import React, { useEffect, useRef } from "react";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 
 export default function Contacto() {
-  const mapRef = useRef();
-  const mapContainerRef = useRef();
+  const mapContainer = useRef(null);
 
   useEffect(() => {
-    mapboxgl.accessToken =
-      "pk.eyJ1IjoiYW5kcmVzMTEyOTgiLCJhIjoiY2wwdmhqZXJqMGo4dTNrbXRobm1pc3JwdiJ9.esLY4egXP8RS0iwrCkl15A";
+    if (!mapContainer.current) return;
 
-    mapRef.current = new mapboxgl.Map({
-      container: mapContainerRef.current,
-      style: "mapbox://styles/andres11298/cl0vhlric000h14qpffv5407k",
-      center: [-75.50307617472883, 6.056310352427602],
-      zoom: 15.18,
-    });
+    const map = L.map(mapContainer.current).setView(
+      [6.056235656557109, -75.5030902713936],
+      14
+    );
 
-    mapRef.current.on("load", () => {
-      mapRef.current.loadImage(
-        "https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png",
-        (error, image) => {
-          if (error) throw error;
-          mapRef.current.addImage("custom-marker", image);
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution: "© OpenStreetMap contributors",
+    }).addTo(map);
 
-          mapRef.current.addSource("points", {
-            type: "geojson",
-            data: {
-              type: "FeatureCollection",
-              features: [
-                {
-                  type: "Feature",
-                  geometry: {
-                    type: "Point",
-                    coordinates: [-75.50307617472883, 6.056310352427602],
-                  },
-                  properties: {
-                    title: "Retro Cafe",
-                  },
-                },
-              ],
-            },
-          });
+    L.marker([6.056235656557109, -75.5030902713936])
+      .addTo(map)
+      .bindPopup("Retro Cafe")
+      .openPopup();
 
-          mapRef.current.addLayer({
-            id: "points",
-            type: "symbol",
-            source: "points",
-            layout: {
-              "icon-image": "custom-marker",
-              "text-field": ["get", "title"],
-              "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
-              "text-offset": [0, 1.25],
-              "text-anchor": "top",
-            },
-          });
-        }
-      );
-    });
+    return () => map.remove();
   }, []);
 
   return (
@@ -70,11 +36,7 @@ export default function Contacto() {
         <div className="w-full bg-slate-900 h-96">
           <h1>Escribenos</h1>
         </div>
-        <div
-          id="map"
-          ref={mapContainerRef}
-          className="w-full h-96 overflow-hidden bg-gray-200  rounded"
-        />
+        <div ref={mapContainer} className="w-full h-96" />
       </div>
       <Footer />
     </>
