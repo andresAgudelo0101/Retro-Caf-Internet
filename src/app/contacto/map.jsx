e9a67ab8bd3b14d4644e127671d4b88e3b862c05
sebastian <echeverry11298@gmail.com>
2025-03-05 15:21:52 -0500

    alparecer el mapa funciona recemos

---
+++ src/app/contacto/map.jsx
@@ -0,0 +1,26 @@
+// components/Map.js
+
+import 'leaflet/dist/leaflet.css';
+import { MapContainer,TileLayer,Marker,Popup } from 'react-leaflet';
+
+// Carga dinámica de los componentes de react-leaflet
+
+const Map = () => {
+  const position = [6.056395704059504, -75.50308690356476]; // Coordenadas de Londres
+
+  return (
+    <MapContainer center={position} zoom={14} className='w-full h-96'>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={position} >
+      <Popup>
+          Retro Cafe Internet
+    </Popup>
+    </Marker>
+    </MapContainer>
+  );
+};
+
+export default Map;
--- src/app/contacto/page.jsx
+++ src/app/contacto/page.jsx
@@ -1,33 +1,10 @@
-"use client";
 
 import Footer from "../components/footer";
 import Header from "../components/header";
-import React, { useEffect, useRef } from "react";
-import L from "leaflet";
-import "leaflet/dist/leaflet.css";
+import Map from ".";
+
 
 export default function Contacto() {
-  const mapContainer = useRef(null);
-
-  useEffect(() => {
-    if (!mapContainer.current) return;
-
-    const map = L.map(mapContainer.current).setView(
-      [6.056235656557109, -75.5030902713936],
-      14
-    );
-
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      attribution: "© OpenStreetMap contributors",
-    }).addTo(map);
-
-    L.marker([6.056235656557109, -75.5030902713936])
-      .addTo(map)
-      .bindPopup("Retro Cafe")
-      .openPopup();
-
-    return () => map.remove();
-  }, []);
 
   return (
     <>
@@ -36,7 +13,7 @@ export default function Contacto() {
         <div className="w-full bg-slate-900 h-96">
           <h1>Escribenos</h1>
         </div>
-        <div ref={mapContainer} className="w-full h-96" />
+        <Map/>
       </div>
       <Footer />
     </>
